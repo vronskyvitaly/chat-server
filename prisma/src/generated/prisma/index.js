076e00c15c4ced5779e0a594ca57bb0faeb2cfb1
vitalyvronsky <vronskyvitaly@mail.ru>
2025-11-10 20:56:50 +0300

    feat: added new chat 2

--- prisma/src/generated/prisma/index.js
+++ prisma/src/generated/prisma/index.js
@@ -202,7 +202,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../..",

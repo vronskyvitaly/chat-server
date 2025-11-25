68fdf099c1a7de81f7acb6fc68432dea02a361a8
vitalyvronsky <vronskyvitaly@mail.ru>
2025-11-25 08:40:08 +0300

    feat: added socket io 6

--- src/router/swagger.ts
+++ src/router/swagger.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
-import { swaggerSpec } from '../../swaggerConfig'
+import { swaggerSpec } from '../swaggerConfig'
 
 const router = express.Router()
 
